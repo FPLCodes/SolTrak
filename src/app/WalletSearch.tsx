d8ecde690cff45d5ee6b372744c98d1358a69f50
Sahab <sahabferdous1@gmail.com>
2024-08-29 19:29:36 +0800

    Add feature to search for other peoples wallets

---
+++ src/app/WalletSearch.tsx
@@ -0,0 +1,85 @@
+import { PublicKey, Connection } from "@solana/web3.js";
+import { useState, useEffect } from "react";
+import BalanceCard from "@/components/balanceCard";
+import TransactionTable from "./transactions/transaction-table";
+import { SearchIcon } from "lucide-react";
+
+const solConversionFactor = 1e9;
+
+// Replace <API KEY> with your actual Alchemy API key
+const connection = new Connection(
+  "https://solana-mainnet.g.alchemy.com/v2/Gm-xaejXqLMg4DNz5NaM1_B6Q81Db5SV"
+);
+
+const WalletSearch = () => {
+  const [address, setAddress] = useState<string>("");
+  const [balance, setBalance] = useState<number | null>(null);
+  const [transactions, setTransactions] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchBalance = async () => {
+    setLoading(true);
+    try {
+      const publicKey = new PublicKey(address.trim());
+      const balance = await connection.getBalance(publicKey);
+      console.log("Fetched Balance (Lamports):", balance);
+      setBalance(balance / solConversionFactor);
+
+      // Fetch recent transaction signatures
+      const signatures = await connection.getSignaturesForAddress(publicKey, {
+        limit: 5,
+      });
+
+      const transactionDetailsPromises = signatures.map(
+        async (signatureInfo) => {
+          const transaction = await connection.getTransaction(
+            signatureInfo.signature,
+            { maxSupportedTransactionVersion: 2 }
+          );
+          return transaction;
+        }
+      );
+
+      const transactions = await Promise.all(transactionDetailsPromises);
+      console.log("Fetched Transactions:", transactions);
+      setTransactions(transactions);
+    } catch (err) {
+      setError("Invalid address or unable to fetch data.");
+      console.error("Error in fetchBalance:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="mx-auto">
+      <div className="flex justify-between mt-4">
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          placeholder="Enter wallet address"
+          className="border-border p-2 w-full mr-4 ring-ring"
+        />
+        <button
+          onClick={() => fetchBalance()}
+          className="button rounded-lg px-2"
+        >
+          <SearchIcon />
+        </button>
+      </div>
+
+      {loading && <p>Loading...</p>}
+      {error && <p className="text-red-500 mt-4">{error}</p>}
+      {balance !== null && (
+        <div className="mt-4">
+          <BalanceCard SOLBalance={balance} USDBalance={balance} />
+          <TransactionTable transactions={transactions} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WalletSearch;

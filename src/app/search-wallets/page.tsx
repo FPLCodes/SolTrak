9d2e91b796080db0b5c76a1c6cd024853310d505
Sahab <sahabferdous1@gmail.com>
2024-09-07 22:47:54 +0800

    Fix typo

--- src/app/search-wallets/page.tsx
+++ src/app/search-wallets/page.tsx
@@ -5,7 +5,7 @@ import WalletSearch from "@/components/WalletSearch";
 
 const SearchWalletsPage = () => {
   return (
-    <div className="mx-auto">
+    <div className="mx-auto w-2/3 mt-6">
       <h1 className="text-2xl font-bold text-center mb-8">Search Wallets</h1>
       <WalletSearch />
     </div>
